a770fc376fe0dff28c8687d6be1021617e69ebe6
frou01 <frou01stg44hurutaka@gmail.com>
2026-07-20 20:09:02 +0900

    namespace修正 GAC連携を分離するのでCallbackOrderを対応させる

--- Editor/Interlock_BuildProcess.cs
+++ Editor/Interlock_BuildProcess.cs
@@ -1,54 +1,54 @@
 ﻿
 using frou01.util;
 using RBUR_SignalIntegrator;
-using UdonSharpEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using VRC.SDKBase;
-using VRC.Udon;
 
-public class Interlock_BuildProcess : IProcessSceneWithReport
+namespace RBUR_SignalIntegrator_Editor
 {
-    //public class Interlock_BuildPreProcess : IProcessSceneWithReport
-    //{
-    //    public int callbackOrder => 0;
-
-    //    public void OnProcessScene(Scene scene, BuildReport report)
-    //    {
-            
-    //    }
-
-    //}
-    public int callbackOrder => 0;
-
-    public void OnProcessScene(Scene scene, BuildReport report)
+    public class Interlock_BuildProcess : IProcessSceneWithReport
     {
-        Debug.Log("InterlockLink Start Process");
-        foreach (GameObject obj in scene.GetRootGameObjects())
-        {
-            foreach (RouteChecker routeChecker in obj.GetComponentsInChildren<RouteChecker>(true))
-            {
-                routeChecker.SetupCallback();
-            }
+        //public class Interlock_BuildPreProcess : IProcessSceneWithReport
+        //{
+        //    public int callbackOrder => 0;
 
-            foreach (Interlocking interlocking in obj.GetComponentsInChildren<Interlocking>(true))
+        //    public void OnProcessScene(Scene scene, BuildReport report)
+        //    {
+
+        //    }
+
+        //}
+        public int callbackOrder => -1;
+
+        public void OnProcessScene(Scene scene, BuildReport report)
+        {
+            Debug.Log("InterlockLink Start Process");
+            foreach (GameObject obj in scene.GetRootGameObjects())
             {
-                interlocking.SetupLocker();
+                foreach (RouteChecker routeChecker in obj.GetComponentsInChildren<RouteChecker>(true))
+                {
+                    routeChecker.SetupCallback();
+                }
+
+                foreach (Interlocking interlocking in obj.GetComponentsInChildren<Interlocking>(true))
+                {
+                    interlocking.SetupLocker();
+                }
+                foreach (InterlockStateLinker stateLinker in obj.GetComponentsInChildren<InterlockStateLinker>(true))
+                {
+                    stateLinker.SetupLocker();
+                }
+                foreach (RouteLocker routeLocker in obj.GetComponentsInChildren<RouteLocker>(true))
+                {
+                    routeLocker.SetupLocker();
+                }
+                //TODO stateLinker,routeLockerからInterlockingへの参照を張る
             }
-            foreach (InterlockStateLinker stateLinker in obj.GetComponentsInChildren<InterlockStateLinker>(true))
-            {
-                stateLinker.SetupLocker();
-            }
-            foreach (RouteLocker routeLocker in obj.GetComponentsInChildren<RouteLocker>(true))
-            {
-                routeLocker.SetupLocker();
-            }
-            //TODO stateLinker,routeLockerからInterlockingへの参照を張る
+            Debug.Log("InterlockLink Process End");
+
         }
-        Debug.Log("InterlockLink Process End");
 
     }
-
 }
