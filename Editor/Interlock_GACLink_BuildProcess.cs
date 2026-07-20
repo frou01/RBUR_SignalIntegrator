e9fe0f2baffd0c31d6327ab63ae28da0d12b37f3
frou01 <frou01stg44hurutaka@gmail.com>
2026-07-20 20:11:42 +0900

    GACリンク用のビルドプロセスを追加

---
+++ Editor/Interlock_GACLink_BuildProcess.cs
@@ -0,0 +1,30 @@
+﻿using frou01.util;
+using RBUR_SignalIntegrator;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RBUR_SignalIntegrator_Editor
+{
+    public class Interlock_GACLink_BuildProcess : IProcessSceneWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnProcessScene(Scene scene, BuildReport report)
+        {
+            Debug.Log("InterlockLink Start Process");
+            foreach (GameObject obj in scene.GetRootGameObjects())
+            {
+                //TODO GACからInterlockへイベントを配送するためPickUpEventLinker/SyncEventLinkerを自動設定する
+
+                foreach (Interlocking interlocking in obj.GetComponentsInChildren<Interlocking>(true))
+                {
+                    interlocking.SetupLocker();
+                }
+            }
+            Debug.Log("InterlockLink Process End");
+
+        }
+    }
+}
